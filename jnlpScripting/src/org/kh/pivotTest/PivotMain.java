057a5642dda07dcb496a311cbbc1875020dbd5b0
Karel Hübl <karel.huebl@gmail.com>
2015-11-10 17:13:43 +0100

    bxml:include mappingFunction problem test case.

---
+++ jnlpScripting/src/org/kh/pivotTest/PivotMain.java
@@ -0,0 +1,50 @@
+package org.kh.pivotTest;
+
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
+
+import pivot_contrib.util.serializer.InjectingSerializer;
+
+public class PivotMain implements Application {
+    private Window window = null;
+
+    @Override
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        BXMLSerializer bxmlSerializer = new InjectingSerializer();
+        this.window = (Window) bxmlSerializer.readObject(Window.class,"/org/kh/pivotTest/Window.bxml");
+        this.window.open(display);
+    }
+    
+    public static String myMappingFunction(String value) {
+    	return value.toUpperCase();
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (this.window != null) {
+            this.window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+        // empty block
+    }
+
+    @Override
+    public void resume() {
+        // empty block
+    }
+
+    // useful to run this as a Java Application in a simpler way (directly)
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(PivotMain.class, args);
+    }
+
+}
